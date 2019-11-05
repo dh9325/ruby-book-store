fdefcdc00024be1f65af15140e6c41f926d9ccc7
dh9325 <dariusz.huk@gmail.com>
2019-11-05 22:45:35 +0000

    Index, show, edit, create publishers

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,28 +1,48 @@
 class PublishersController < ApplicationController
+  def index
+    @page_title = 'Available Publishers'
+    @publishers = Publisher.all
+  end
+
+  def show
+    @page_title = 'View Publisher'
+    @publisher = Publisher.find(params[:id])
+  end
+
   def new
-  end
-
-  def create
-  end
-
-  def update
+    @page_title = 'New Publisher'
+    @publisher = Publisher.new
   end
 
   def edit
+    @page_title = 'Edit Publisher'
+    @publisher = Publisher.find(params[:id])
+  end
+
+  def create
+    @publisher = Publisher.new(publisher_params)
+    if @publisher.save
+      redirect_to publishers_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @publisher = Publisher.find(params[:id])
+    if @publisher.update(publisher_params)
+      redirect_to @publisher
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   end
 
-  def index
-  end
-
-  def show
-  end
-
   private
 
-  def publisher_params
-    params.require(:publisher).permit(:name)
-  end
+    def publisher_params
+      params.require(:publisher).permit(:name)
+    end
 end
