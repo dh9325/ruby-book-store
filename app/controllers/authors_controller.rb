209bf5666a0266fe78f4df8b54005d8181c639b3
dh9325 <dariusz.huk@gmail.com>
2019-11-05 22:35:00 +0000

    Index, show, edit, create authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,24 +1,47 @@
 class AuthorsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
   def index
+    @page_title = 'Available Authors'
+    @authors = Author.all
   end
 
   def show
+    @page_title = 'View Author'
+    @author = Author.find(params[:id])
   end
+
+  def new
+    @page_title = 'New Author'
+    @author = Author.new
+  end
+
+  def edit
+    @page_title = 'Edit Author'
+    @author = Author.find(params[:id])
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to authors_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+  end
+
 
   private
 
