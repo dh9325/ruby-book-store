e4f28ce39280a3bab55c270c4b3bc3e958d89a74
dh9325 <dariusz.huk@gmail.com>
2019-11-18 23:20:38 +0000

    Complete adding new book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   belongs_to :category
   belongs_to :author
   belongs_to :publisher
+
+  validates :title, :isbn, :year, :price, :buy, :exceprt, :format, :pages, presence: true
 end
